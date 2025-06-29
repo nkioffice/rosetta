40a9166af1b068c59ed69a2391f30ca873ad80d9
nki <nkioffice.llc@gmail.com>
2025-06-29 23:08:05 +0900

    sql

--- rosetta.sql
+++ rosetta.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- ホスト: 127.0.0.1
--- 生成日時: 2025-06-27 05:48:24
--- サーバのバージョン： 10.4.32-MariaDB
--- PHP のバージョン: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: 
+-- サーバのバージョン： 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- データベース: `rosetta`
+-- Database: `rosetta`
 --
 
 -- --------------------------------------------------------
@@ -31,7 +32,7 @@ CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(100) NOT NULL,
   `categoty_path` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -43,9 +44,9 @@ CREATE TABLE `items` (
   `item_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `discounted_price` int(11) NOT NULL DEFAULT 0,
+  `discounted_price` int(11) NOT NULL DEFAULT '0',
   `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `items`
@@ -63,7 +64,7 @@ INSERT INTO `items` (`item_id`, `name`, `price`, `discounted_price`, `descriptio
 CREATE TABLE `item_categories` (
   `item_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -75,39 +76,104 @@ CREATE TABLE `item_images` (
   `item_id` int(11) NOT NULL,
   `filename` varchar(100) NOT NULL,
   `num` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- ダンプしたテーブルのインデックス
+-- テーブルのデータのダンプ `item_images`
+--
+
+INSERT INTO `item_images` (`item_id`, `filename`, `num`) VALUES
+(1, 'snake1.jpg', 2),
+(1, 'snake2.jpg', 1),
+(1, 'snake3.jpg', 3);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `item_reviews`
+--
+
+CREATE TABLE `item_reviews` (
+  `review_id` int(11) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `stars` float NOT NULL,
+  `text` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `item_reviews`
+--
+
+INSERT INTO `item_reviews` (`review_id`, `title`, `item_id`, `stars`, `text`, `created_at`) VALUES
+(1, '細いのに圧倒的存在感', 1, 5, '細いのに立体感があり、素肌に付けているだけでとても目立ちます。 今までネックレスを付けていても誰かに反応されることはなかったのですが、ROSETTAのネックレスは存在感があって、色んな人に「それどこの？」と 聞かれます。\r\n\r\n', '2025-06-29 20:22:01'),
+(2, '翌日には届いた', 1, 5, '後払いで購入し、翌日には手元に届きました！\r\n彼氏へのプレゼント用で購入しましたが、 箱も高級感があって、品質保証書も付いていたので、 プレゼント用に最適だと思いました！ 次はお揃いで購入しようと思います！', '2025-06-29 20:22:26'),
+(3, '軽い', 1, 5, '一日中つけても重さを感じず、寝るときも気になりません。本当に身体の一部になったかのようです。', '2025-06-29 20:45:09'),
+(4, 'ギフト映え', 1, 5, 'しっかりとした箱に品質保証書も同封されていて、高級感が伝わるギフトになりました。', '2025-06-29 20:45:09'),
+(5, '2連に！', 1, 4, '別ブランドのペンダントと二連にして着けています。チェーンのしなやかさに感動しました。', '2025-06-29 20:46:22');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `review_images`
+--
+
+CREATE TABLE `review_images` (
+  `review_id` int(11) NOT NULL,
+  `filename` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `review_images`
+--
+
+INSERT INTO `review_images` (`review_id`, `filename`) VALUES
+(3, 'sample.jpg');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- テーブルのインデックス `categories`
+-- Indexes for table `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- テーブルのインデックス `items`
+-- Indexes for table `items`
 --
 ALTER TABLE `items`
   ADD PRIMARY KEY (`item_id`);
 
 --
--- ダンプしたテーブルの AUTO_INCREMENT
+-- Indexes for table `item_reviews`
+--
+ALTER TABLE `item_reviews`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- テーブルの AUTO_INCREMENT `categories`
+-- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- テーブルの AUTO_INCREMENT `items`
+-- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
   MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `item_reviews`
+--
+ALTER TABLE `item_reviews`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
